0c7b71d7f5c30743105fa546ac33749a493d1765
cybershipsdevelopment <2024.scott.pawley@gmail.com>
2022-12-26 13:30:12 -0500

    Update GUI

--- test.py
+++ test.py
@@ -1,35 +1,45 @@
 import tkinter as tk
 
-class MainWindow(tk.Frame):
-    def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
+class MainWindow(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        # create a frame for the main menu
-        self.main_menu = tk.Frame(self)
-        self.main_menu.pack()
+        # create main frame
+        self.main_frame = tk.Frame(self)
+        self.main_frame.pack(side='top', fill='both', expand=True)
 
-        # create a button for switching to the settings frame
-        self.settings_button = tk.Button(self.main_menu, text="Settings", command=self.open_settings)
-        self.settings_button.pack()
+        # create left sidebar
+        self.left_sidebar = tk.Frame(self, bg='gray')
+        self.left_sidebar.pack(side='left', fill='y')
 
-        # create a frame for the settings
-        self.settings = tk.Frame(self)
+        # create right sidebar
+        self.right_sidebar = tk.Frame(self, bg='gray')
+        self.right_sidebar.pack(side='right', fill='y')
 
-        # create a button for switching back to the main menu
-        self.back_button = tk.Button(self.settings, text="Back", command=self.open_main_menu)
-        self.back_button.pack()
+class Game(MainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    def open_settings(self):
-        # hide the main menu and show the settings
-        self.main_menu.pack_forget()
-        self.settings.pack()
+        # create centered frame for text box and entry widget
+        self.centered_frame = tk.Frame(self.main_frame)
+        self.centered_frame.pack(side='top', fill='x')
 
-    def open_main_menu(self):
-        # hide the settings and show the main menu
-        self.settings.pack_forget()
-        self.main_menu.pack()
+        # add text box to centered frame
+        self.text_box = tk.Text(self.centered_frame)
+        self.text_box.pack(side='top', fill='x')
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    MainWindow(root).pack()
-    root.mainloop()
+        # add entry widget to centered frame
+        self.entry = tk.Entry(self.centered_frame)
+        self.entry.pack(side='top', fill='x')
+
+        # add widgets to left sidebar
+        self.left_sidebar_label = tk.Label(self.left_sidebar, text='Left Sidebar')
+        self.left_sidebar_label.pack()
+
+        # add widgets to right sidebar
+        self.right_sidebar_label = tk.Label(self.right_sidebar, text='Right Sidebar')
+        self.right_sidebar_label.pack()
+
+if __name__ == '__main__':
+    app = Game()
+    app.mainloop()
--- user_interface.py
+++ user_interface.py
@@ -21,11 +21,11 @@ class App(tk.Tk):
         self.frame.pack()
 
         # Set a keybind to switch frames.
-        self.bind("<Key>", self.on_keypress)
+        self.bind("<Key>", self.delete_splash)
         self.used = False
         
     # Switch frames when the user presses a key.
-    def on_keypress(self, event):
+    def delete_splash(self, event):
         if self.used == False:
             self.switch_frame(Main)
             self.used = True
@@ -36,13 +36,13 @@ class App(tk.Tk):
     def switch_frame(self, newFrame):
         self.frame.pack_forget()
         self.frame = newFrame(self)
-        self.frame.grid(row=0, column=0)
+        self.frame.pack(anchor=tk.CENTER)
 
 class SplashScreen(tk.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, *args, **kwargs):
 
         # Inherit from the frame class.
-        super().__init__(parent)
+        super().__init__(parent, *args, **kwargs)
 
         # Open the original splash screen image and create a reference to it.
         original_image = Image.open("graphics/splash_screen.png")
@@ -58,71 +58,147 @@ class SplashScreen(tk.Frame):
         label.pack()
 
 class Main(tk.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, *args, **kwargs):
 
         # Inherit from the frame class.
-        super().__init__(parent)
+        super().__init__(parent, *args, **kwargs)
         
-        # Display the player stats in the top left corner.
-        self.player_stats = PlayerStats(self)
-        self.player_stats.grid(row=0, column=0, sticky=tk.W)
+        # Display the left sidebar.
+        self.left_sidebar = LeftSidebar(self, bg="dark gray")
+        self.left_sidebar.pack(side=tk.LEFT, fill=tk.Y)
+
+        # Display the right sidebar.
+        self.right_sidebar = RightSidebar(self, bg="dark gray")
+        self.right_sidebar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        # Display the main window.
+        self.main_window = MainWindow(self, bg="white")
+        self.main_window.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+class LeftSidebar(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+
+        # Inherit from the frame class.
+        super().__init__(parent, *args, **kwargs)
+
+        # Configure the frame.
+        self.background = kwargs.get("bg")
+        self.configure(bg=self.background)
+
+        # Display the player stats in the top.
+        self.player_stats = PlayerStats(self, bg = self.background)
+        self.player_stats.grid(row=0, column=0, sticky=tk.NSEW)
 
         # Display the inventory below the player stats.
-        self.inventory = Inventory(self)
-        self.inventory.grid(row=1, column=0, sticky=tk.W)
+        self.inventory = Inventory(self, bg = self.background)
+        self.inventory.grid(row=1, column=0, sticky=tk.NSEW)
+
+class MainWindow(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+
+        # Inherit from the frame class.
+        super().__init__(parent, *args, **kwargs)
+
+        # Configure the frame.
+        self.background = kwargs.get("bg")
+        self.configure(bg=self.background)
+
+        # Display a text entry box at the bottom.
+        self.text_entry = tk.Entry(self, bg=self.background, font=MAINFONT)
+        self.text_entry.pack(side=tk.BOTTOM, fill=tk.X)
+        self.text_entry.bind("<Return>", self.get_text)
+
+        # Display a text box at the top.
+        self.text_box = tk.Text(self, bg="gray", font=MAINFONT)
+        self.text_box.pack(side=tk.TOP, fill=tk.X)
+        self.text_box.insert(tk.END, "Welcome to the game!")
+        self.text_box.config(state=tk.DISABLED)
+
+        
+
+    def get_text(self, event):
+        text = self.text_entry.get()
+        self.text_entry.delete(0, tk.END)
+        self.text_box.config(state=tk.NORMAL)
+        self.text_box.insert(tk.END, text)
+        self.text_box.config(state=tk.DISABLED)
+        self.text_box.see(tk.END)
+        self.text_entry.focus()
+
+class RightSidebar(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+
+        # Inherit from the frame class.
+        super().__init__(parent, *args, **kwargs)
+
+        # Configure the frame.
+        self.background = kwargs.get("bg")
+        self.configure(bg=self.background)
+
+        # Display the inventory below the player stats.
+        self.inventory = Inventory(self, bg = self.background)
+        self.inventory.pack(side=tk.TOP)
 
 class PlayerStats(tk.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, *args, **kwargs):
         
         # Inherit from the frame class.
-        super().__init__(parent)
+        super().__init__(parent, *args, **kwargs)
 
-        # Display the title of the player stats frame.
-        self.title = tk.Label(self, text="Player Stats:", font=MAINFONT)
-        self.title.grid(row=0, column=0, sticky=tk.W)
+        # Configure the frame.
+        self.background = kwargs.get("bg")
+        self.configure(bg=self.background)
 
-        # Display an invisible divider.
-        self.divider = tk.Label(self, text="", font=MAINFONT)
-        self.divider.grid(row=1, column=0, sticky=tk.W)
+        # Create a label for the player's name.
+        self.name_label = tk.Label(self, text="Placeholder", font=MAINFONT, bg=self.background)
+        self.name_label.grid(row=0, column=0, sticky=tk.W)
 
         # Display the health of the player.
-        self.health = tk.Label(self, text="Health: 100/100", font=MAINFONT)
-        self.health.grid(row=2, column=0, sticky=tk.W)
+        self.health = tk.Label(self, text="Health: 100/100", font=MAINFONT, bg=self.background)
+        self.health.grid(row=1, column=0, sticky=tk.W)
 
         # Display the mana of the player.
-        self.mana = tk.Label(self, text="Mana: 100/100", font=MAINFONT)
-        self.mana.grid(row=3, column=0, sticky=tk.W)
+        self.mana = tk.Label(self, text="Mana: 100/100", font=MAINFONT, bg=self.background)
+        self.mana.grid(row=2, column=0, sticky=tk.W)
 
         # Display an invisible divider.
-        self.divider = tk.Label(self, text="", font=MAINFONT)
-        self.divider.grid(row=4, column=0, sticky=tk.W)
+        self.divider = tk.Label(self, text="", font=MAINFONT, bg=self.background)
+        self.divider.grid(row=3, column=0, sticky=tk.W)
 
 class Inventory(tk.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, *args, **kwargs):
 
         # Inherit from the frame class.
-        super().__init__(parent)
+        super().__init__(parent, *args, **kwargs)
+
+        # Configure the frame.
+        self.background = kwargs.get("bg")
+        self.configure(bg=self.background)
 
         # Display the title of the inventory frame.
-        self.title = tk.Label(self, text="Inventory:", font=MAINFONT)
+        self.title = tk.Label(self, text="Inventory:", font=MAINFONT, bg=self.background)
         self.title.grid(row=0, column=0, sticky=tk.W)
 
         # Create scrolling menu with inventory items.
-        self.inventory = InventoryBox(self)
+        self.inventory = InventoryBox(self, bg=self.background)
         self.inventory.grid(row=1, column=0, sticky=tk.W)
 
 class InventoryBox(tk.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, *args, **kwargs):
 
         # Inherit from the frame class.
-        super().__init__(parent)
+        super().__init__(parent, *args, **kwargs)
+
+        # Configure the frame.
+        self.background = kwargs.get("bg")
+        self.configure(bg=self.background)
 
         # Display a scrollbar on the right side of the frame.
         self.scrollbar = tk.Scrollbar(self)
         self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
         # Display the contents of the inventory.
-        self.contents = tk.Listbox(self, width=32, height=33, yscrollcommand=self.scrollbar.set)
+        self.contents = tk.Listbox(self, width=36, height=36, bg="light gray", yscrollcommand=self.scrollbar.set)
         for line in range(100):
             self.contents.insert(tk.END, "Item " + str(line))
         self.contents.pack(side=tk.LEFT, fill=tk.BOTH)
