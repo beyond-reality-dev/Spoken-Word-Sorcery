62ac8beb762077681ffd3b76c86613e7884553de
beyond-reality-dev <79934131+beyond-reality-dev@users.noreply.github.com>
2024-07-20 21:46:57 -0400

    Test mapping system and work on continuing intro

--- app/src/cutscenes/grandHall/grandHall.js
+++ app/src/cutscenes/grandHall/grandHall.js
@@ -2,19 +2,23 @@ const { printLines, requireAnswer } = require("../../general");
 const { getValue } = require("../../save_data");
 
 async function grandHall() {
-  if (getValue("barracks", true).isVisited == false) {
-    printLines("app/src/cutscenes/grandHall/1.txt");
-    return;
+  if (getValue("armory", true).isVisited == true && getValue("militaryAnnex", true).isVisited == true) {
+    if (getValue("barracks", true).isVisited == false) {
+      printLines("app/src/cutscenes/grandHall/1.txt");
+      return;
+    } else {
+      printLines("app/src/cutscenes/grandHall/2.txt");
+    }
+    await requireAnswer(["yes", "y"], '"I want the truth," he said. "Is this everyone?"');
+    printLines("app/src/cutscenes/grandHall/3.txt");
+    await requireAnswer(["yes", "y"], '"I am afraid you do not have much of a choice," he scolded. "So I will ask again, are you ready?');
+    printLines("app/src/cutscenes/grandHall/4.txt");
+    await requireAnswer(["no", "n"], "You try to speak, but the words catch in your throat. Your oath requires you to answer honestly.");
+    printLines("app/src/cutscenes/grandHall/5.txt");
+    await requireAnswer(["yes", "y"], '"Is that clear?"');
   } else {
-    printLines("app/src/cutscenes/grandHall/2.txt");
+    printLines("app/src/cutscenes/grandHall/6.txt");
   }
-  await requireAnswer(["yes", "y"], '"I want the truth," he said. "Is this everyone?"');
-  printLines("app/src/cutscenes/grandHall/3.txt");
-  await requireAnswer(["yes", "y"], '"I am afraid you do not have much of a choice," he scolded. "So I will ask again, are you ready?');
-  printLines("app/src/cutscenes/grandHall/4.txt");
-  await requireAnswer(["no", "n"], "You try to speak, but the words catch in your throat. Your oath requires you to answer honestly.");
-  printLines("app/src/cutscenes/grandHall/5.txt");
-  await requireAnswer(["yes", "y"], '"Is that clear?"');
 }
 
 module.exports = { grandHall };
