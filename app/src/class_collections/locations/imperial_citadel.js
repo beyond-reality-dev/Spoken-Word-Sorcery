75f4d1273b09f3f3731c262279fa4b7baf2a5a2d
beyond-reality-dev <79934131+beyond-reality-dev@users.noreply.github.com>
2024-07-24 16:41:28 -0400

    Continue working on map outside of the academy

--- app/src/class_collections/locations/imperial_citadel.js
+++ app/src/class_collections/locations/imperial_citadel.js
@@ -16,17 +16,42 @@ class SurvivorCamp extends Room {
   }
 }
 
+var survivorCamp = new SurvivorCamp();
+
 class AcademyEntrance extends Room {
   constructor() {
     super(
       "Academy Entrance",
       "academyEntrance",
-      "The entrance to the Imperial Academy is a large, open area. There is a path to the north, leading to the survivor camp.",
+      "The entrance to the Imperial Academy is a large, open area. The remains of large, iron gates lie shattered in the burnt grass. There is a path to the north, leading to the survivor camp, and a road to the south, leading to the Academy's bridge.",
       "10",
       "10"
     );
     this.exits = {
       north: "survivorCamp",
+      south: "academyBridge"
     };
   }
 }
+
+var academyEntrance = new AcademyEntrance();
+
+class AcademyBridge extends Room {
+  constructor() {
+    super(
+      "Academy Bridge",
+      "academyBridge",
+      "The Academy Bridge is a long, stone bridge that spans a deep moat. There is a path to the north, leading back to the Academy Entrance, and a path to the south, leading to the Imperial Citadel.",
+      "10",
+      "10"
+    );
+    this.exits = {
+      north: "academyEntrance",
+      south: "imperialCitadel"
+    };
+  }
+}
+
+var academyBridge = new AcademyBridge();
+
+module.exports = { survivorCamp, academyEntrance, academyBridge };
