6d9e2bc5902497a96bfbdf20114d6921e6291131
beyond-reality-dev <79934131+beyond-reality-dev@users.noreply.github.com>
2024-07-24 15:40:33 -0400

    Start working on area outside of the academy

--- app/src/class_collections/locations/imperial_citadel.js
+++ app/src/class_collections/locations/imperial_citadel.js
@@ -1,1 +1,32 @@
 const { Room } = require("../locations/room")
+
+class SurvivorCamp extends Room {
+  constructor() {
+    super(
+      "Survivor Camp",
+      "survivorCamp",
+      "The survivor camp is a small camp with a few tents and a campfire. There is a path to the north, leading back to the Imperial Academy.",
+      "10",
+      "10"
+    );
+    this.exits = {
+      north: "restOfAcademy",
+      south: "academyEntrance",
+    };
+  }
+}
+
+class AcademyEntrance extends Room {
+  constructor() {
+    super(
+      "Academy Entrance",
+      "academyEntrance",
+      "The entrance to the Imperial Academy is a large, open area. There is a path to the north, leading to the survivor camp.",
+      "10",
+      "10"
+    );
+    this.exits = {
+      north: "survivorCamp",
+    };
+  }
+}
